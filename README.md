2589c4c34485a42747ce3727fb2e510f0cebebcc
Harrison Hur <11700278+harhur@users.noreply.github.com>
2018-02-10 01:14:15 -0500

    Add revised files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Mailendar
+A personal project that helps you organize your life one click at a time
---
+++ popup.js
@@ -0,0 +1,58 @@
+var x = new XMLHttpRequest();
+x.open("GET", "https://apis.google.com/js/platform.js");
+x.send();
+
+// used for testing
+var userID = "harrisonhur@gmail.com";
+
+// retrieves a list of messages given string userId, string query, function callback
+// callback function is a function that is passed to another function
+
+function listMessages(userId, query, callback) {
+  var getPageOfMessages = function(request, result) {
+    request.execute(function(resp) {
+      result = result.concat(resp.messages);
+      var nextPageToken = resp.nextPageToken;
+      if (nextPageToken) {
+        request = gapi.client.gmail.users.messages.list({
+          'userId': userId,
+          'pageToken': nextPageToken,
+          'q': query
+        });
+        getPageOfMessages(request, result);
+      } else {
+        callback(result);
+      }
+    });
+  };
+  var initialRequest = gapi.client.gmail.users.messages.list({
+    'userId': userId,
+    'q': query
+  });
+  getPageOfMessages(initialRequest, []);
+}
+
+// Get Message with given ID.
+
+function getMessage(userId, messageId, callback) {
+  var request = gapi.client.gmail.users.messages.get({
+    'userId': userId,
+    'id': messageId
+  });
+  request.execute(callback);
+}
+
+// placeholder
+
+function refreshPage() {
+  console.log("hey");
+  document.getElementById("email_list").innerHTML = "Refreshing...";
+}
+
+/*document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('button').addEventListener('click', refreshPage);
+});*/
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('button').addEventListener('click', refreshPage);
+});
